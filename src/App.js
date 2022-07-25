8a9bd6082f6a4088dfed56c4954824f256c422f5
Roman Azeem <romanazeem007@gmail.com>
2022-07-25 07:40:45 +0500

    router config modified and domain added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 const AppRoutes = React.lazy(() => import("AppRoutes"));
 
 
